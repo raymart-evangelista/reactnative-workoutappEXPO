393cf8d5f53f8b0a4b0cd6adf942de4fb5b77ac9
raymart-evangelista <raymart.s.evangelista@gmail.com>
2023-01-27 10:28:49 -0800

    add App.js code

--- App.js
+++ App.js
@@ -1,20 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useEffect, useState } from 'react';
+import { Appearance, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  const [colorScheme, setColorScheme] = useState(Appearance.getColorScheme())
+  const themeStyles = colorScheme === 'dark' ? darkStyles : lightStyles
+
+  useEffect(() => {
+    const themeSubscription = Appearance.addChangeListener(({ colorScheme }) => {
+      setColorScheme(colorScheme)
+    })
+
+    return () => themeSubscription.remove()
+  })
+
+  console.log(colorScheme)
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View style={themeStyles.container}>
       <StatusBar style="auto" />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
