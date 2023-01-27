dd9ffdcaf84730493f009e6aeb21f26d5a396121
raymart-evangelista <raymart.s.evangelista@gmail.com>
2023-01-27 11:44:09 -0800

    add working main

--- App.js
+++ App.js
@@ -1,6 +1,8 @@
 import { StatusBar } from 'expo-status-bar';
 import { useEffect, useState } from 'react';
 import { Appearance, StyleSheet, Text, View } from 'react-native';
+import { lightStyles, darkStyles } from './globalStyles';
+import MainLoggedOut from './MainLoggedOut';
 
 export default function App() {
   const [colorScheme, setColorScheme] = useState(Appearance.getColorScheme())
@@ -18,6 +20,7 @@ export default function App() {
 
   return (
     <View style={themeStyles.container}>
+      <MainLoggedOut />
       <StatusBar style="auto" />
     </View>
   );
---
+++ MainLoggedOut.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { View, Text, TouchableOpacity } from 'react-native'
+
+export default function MainLoggedOut() {
+  return (
+    <View>
+      <TouchableOpacity
+
+      >
+        <Text>Create an account</Text>
+      </TouchableOpacity>
+    </View>
+  )
+}
