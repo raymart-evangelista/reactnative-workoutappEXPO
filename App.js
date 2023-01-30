c8b3083d2f69d48f92c527149d29ee820e5593a2
raymart-evangelista <raymart.s.evangelista@gmail.com>
2023-01-29 22:48:45 -0800

    add init params

--- App.js
+++ App.js
@@ -77,7 +77,7 @@ export default function App() {
       </View> */}
       <Stack.Navigator initialRouteName='Home'>
         <Stack.Screen name="Home" component={HomeScreen} options={{ title: 'Overview' }} />
-        <Stack.Screen name="Details" component={DetailsScreen} />
+        <Stack.Screen name="Details" component={DetailsScreen} initialParams={{ itemId: 443 }} />
       </Stack.Navigator>
     </NavigationContainer>
   );
