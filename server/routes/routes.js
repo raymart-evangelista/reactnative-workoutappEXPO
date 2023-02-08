62326293ab85536b6253877706918760fbebb381
raymart-evangelista <raymart.s.evangelista@gmail.com>
2023-02-08 10:17:52 -0800

    add model and update post method

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -1,12 +1,26 @@
 const express = require('express');
 
 const router = express.Router()
+const Model = require('../models/model')
 
 module.exports = router;
 
 // post
-router.post('/post', (req, res) => {
-  res.send('Post API')
+router.post('/post', async (req, res) => {
+  // res.send('Post API')
+  const data = new Model({
+    name: req.body.name,
+    age: req.body.age
+  })
+
+  try {
+    const dataToSave = await data.save()
+    res.status(200).json(dataToSave)
+
+  } catch (error) {
+    res.status(400).json({ message: error.message })
+
+  }
 })
 
 // get all
@@ -16,7 +30,7 @@ router.get('/getAll', (req, res) => {
 
 // get by ID method
 router.get('/getOne/:id', (req, res) => {
-  res.send('Get by ID API')
+  res.send(req.params.id)
 })
 
 // update by ID method
