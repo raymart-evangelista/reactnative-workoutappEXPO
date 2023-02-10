d72467e37deb98fd4f76b8511c1a1c17b6dfebbc
raymart-evangelista <raymart.s.evangelista@gmail.com>
2023-02-10 11:24:01 -0800

    turn code into async/await

--- src/services/api.js
+++ src/services/api.js
@@ -5,8 +5,17 @@ const getAll = () => {
 
 }
 
-const signUp = (username, email, password) => {
-  return axios.post(`${baseUrl}/signup`, { username, email, password })
+// const signUp = (username, email, password) => {
+//   return axios.post(`${baseUrl}/signup`, { username, email, password })
+// }
+
+const signUp = async (username, email, password) => {
+  try {
+    const response = await axios.post(`${baseUrl}/signup`, { username, email, password })
+    return response.data
+  } catch (error) {
+    console.error(error)
+  }
 }
 
 export default {
