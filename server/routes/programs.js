a6a55a220d6305b11f935f0ede5aa7f6a3e203d1
raymart-evangelista <raymart.s.evangelista@gmail.com>
2023-02-22 11:23:28 -0800

    add 'get program by id'

--- server/routes/programs.js
+++ server/routes/programs.js
@@ -3,6 +3,7 @@ const express = require('express')
 const programsRouter = express.Router()
 const Program = require('../models/program')
 
+// post
 programsRouter.post('/', async (req, res) => {
   try {
     const { name, weeks, weekDetails } = req.body
@@ -20,12 +21,27 @@ programsRouter.post('/', async (req, res) => {
   }
 })
 
+// get all
 programsRouter.get('/', async (req, res) => {
   try {
     const programs = await Program.find()
     res.json(programs)
   } catch (error) {
     res.status(500).json({ message: error.message })
+  }
+})
+
+// get by ID
+programsRouter.get('/:id', async (req, res) => {
+  try {
+    const program = await Program.findById(req.params.id)
+    if (program) {
+      res.json(program)
+    } else {
+      res.status(404).end()
+    }
+  } catch (error) {
+    res.status(400).send({ error: 'malformatted id'})
   }
 })
 
