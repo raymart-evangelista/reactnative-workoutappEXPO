657fde62440914d9ff8b7a750e7fdb0f9d30d7f0
raymart-evangelista <raymart.s.evangelista@gmail.com>
2023-03-02 23:24:32 -0800

    set up formik tutorial

--- src/screens/CreateProgramScreen.js
+++ src/screens/CreateProgramScreen.js
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react"
 import { Button, Modal, Text, TextInput, TouchableOpacity, View } from "react-native"
+import { Formik } from "formik";
 
 import programsService from "../services/programs";
 
@@ -23,6 +24,59 @@ export function ProgramNameInputScreen({ navigation }) {
 
   return (
     <View>
+      <Formik
+        initialValues={{ email: '', password: ''}}
+        validate={values => {
+          const errors = {}
+          if (!values.email) {
+            errors.email = 'Required'
+          } else if (
+            !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
+          ) {
+            errors.email = 'Invalid email address'
+          }
+          return errors
+        }}
+        onSubmit={(values, { setSubmitting }) => {
+          setTimeout(() => {
+            alert(JSON.stringify(values, null, 2))
+            setSubmitting(false)
+          }, 400)
+        }}
+      >
+        {({
+          values,
+          errors,
+          touched,
+          handleChange,
+          handleBlur,
+          handleSubmit,
+          isSubmitting,
+        }) => (
+          <form onSubmit={handleSubmit}>
+            <input 
+              type="email"
+              name="email"
+              onChange={handleChange}
+              onBlur={handleBlur}
+              value={values.email}
+            />
+            {errors.email && touched.email && errors.email}
+            <input 
+              type="password"
+              name="password"
+              onChange={handleChange}
+              onBlur={handleBlur}
+              values={values.password}
+            />
+            {errors.password && touched.password && errors.password}
+            <Button
+              title="Submit"
+              disabled={isSubmitting}
+            />
+          </form>
+        )}
+      </Formik>
       <Text>What is the name of your workout program?</Text>
       <TextInput 
         placeholder="Workout program name"
