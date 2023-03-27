bb3bd50b4f8aebf5080aec7bb8f55de521547ed0
raymart-evangelista <raymart.s.evangelista@gmail.com>
2023-03-27 10:51:29 -0700

    remove unused code

--- src/screens/CreateProgramScreen.js
+++ src/screens/CreateProgramScreen.js
@@ -73,23 +73,6 @@ const Days = ({ weekIndex, week, name, handleChange, setFieldValue }) => (
           </View>
         )}
         {week.dayDetails && week.dayDetails.length < 7 ? (
-          // <TouchableOpacity 
-          //   onPress={() => arrayHelpers.push(
-          //     {
-          //       name: '',
-          //       dayNum: week.dayDetails.length + 1,
-          //       exercises: [],
-          //     }
-          //   )}
-          //   style={{
-          //     backgroundColor: 'green',
-          //     borderRadius: 5,
-          //     padding: 10,
-          //     alignItems: 'center',
-          //   }}
-          // >
-          //   <Text style={{ color: 'white', fontWeight: 'bold' }}>Add a day</Text>
-          // </TouchableOpacity>
         <View style={{ alignItems: 'flex-start', justifyContent: 'center' }}>
           <Button
             icon='plus'
