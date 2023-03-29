f50c225508e550eddbfa7a155aa90cc16bb975ed
raymart-evangelista <raymart.s.evangelista@gmail.com>
2023-03-28 21:10:01 -0700

    remove unused code

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -3,33 +3,8 @@ import { Button, StyleSheet, Text, View } from 'react-native'
 import { defaultStyles } from '../styles/globalStyles';
 
 export default function HomeScreen({ navigation, route }) {
-  useEffect(() => {
-    if (route.params?.post) {
-
-    }
-
-    navigation.setOptions({
-      headerRight: () => (
-        <Button onPress={() => setCount((c) => c + 1)} title="Update count" />
-      )
-    })
-  }, [route.params?.post, navigation])
   return (
     <View style={defaultStyles.basic}>
-      {/* <Text>Home Screen</Text> */}
-      {/* <Button
-        title="Go to Details"
-        onPress={() => {
-          navigation.navigate('Details', {
-            itemId: 86,
-            otherParam: 'anything you want here',
-          })
-        }}
-      /> */}
-      {/* <Button 
-        title="Create post"
-        onPress={() => navigation.navigate('CreatePost')}
-      /> */}
       <Button 
         title="Create program"
         onPress={() => navigation.navigate('CreateProgram')}
@@ -41,8 +16,6 @@ export default function HomeScreen({ navigation, route }) {
       <Button 
         title="Continue program"
       />
-      {/* <Text style={{ margin: 10 }}>Post: {route.params?.post}</Text> */}
-      {/* <Text>Count: {count}</Text> */}
     </View>
   )
 }
