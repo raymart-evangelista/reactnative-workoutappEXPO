c45e3e30cd235ad765e2b5f8c446b0f55d2cc699
raymart-evangelista <raymart.s.evangelista@gmail.com>
2023-04-13 12:46:07 -0700

    fix defaults, add multiline and numberOfLines support

--- src/components/TextInput.js
+++ src/components/TextInput.js
@@ -1,15 +1,17 @@
 import { TextInput as PaperTextInput } from 'react-native-paper'
 
-const TextInput = ({field, label, placeholder, style}) => {
+const TextInput = ({ field, label, placeholder, style, multiline = false, numberOfLines = 1 }) => {
   return (
     <PaperTextInput
       mode='outlined'
       label={label}
+      multiline={multiline}
+      numberOfLines={numberOfLines}
       onChangeText={field.onChange(field.name)}
       onBlur={field.onBlur(field.name)}
       value={field.value ? field.value.toString() : ''}
       placeholder={placeholder}
-      style={style}
+      style={{ maxHeight: 240 }}
     />
   )
 }
