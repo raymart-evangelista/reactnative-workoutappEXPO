ad09abe57a44a6aeba5190a7b1331efb3f619efa
raymart-evangelista <raymart.s.evangelista@gmail.com>
2023-05-10 12:43:19 -0700

    Revert "install continued"
    This reverts commit f9d86fcf43d31fd0fd8222f955b5e9ce2d5624e2.

--- babel.config.js
+++ babel.config.js
@@ -2,15 +2,6 @@ module.exports = function(api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
-    plugins: [
-      [
-        'module:react-native-dotenv',
-        {
-          moduleName: 'react-native-dotenv',
-          path: '.env',
-        }
-      ]
-    ],
     env: {
       production: {
         plugins: ['react-native-paper/babel'],
