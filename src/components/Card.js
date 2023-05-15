893c417ad1dda1f9278d472d5cef76169e5c4cd8
raymart-evangelista <raymart.s.evangelista@gmail.com>
2023-05-14 18:09:38 -0700

    add react-native-paper Card personal component

---
+++ src/components/Card.js
@@ -0,0 +1,24 @@
+import * as React from 'react'
+
+import { Avatar, Button, Card as PaperCard, Text } from 'react-native-paper'
+
+const LeftComponent = props => <Avatar.Icon {...props} icon="folder" />
+
+const Card = () => {
+  return (
+    <PaperCard>
+      <PaperCard.Title title="Card Title" subtitle="Card Subtitle" left={LeftComponent} />
+      <PaperCard.Content>
+        <Text variant="titleLarge">Card title</Text>
+        <Text variant="bodyMedium">Card content</Text>
+      </PaperCard.Content>
+      <PaperCard.Cover source={{ uri: 'https://picsum.photos/700' }} />
+      <PaperCard.Actions>
+        <Button>Cancel</Button>
+        <Button>Ok</Button>
+      </PaperCard.Actions>
+    </PaperCard>
+  )
+}
+
+export default Card
