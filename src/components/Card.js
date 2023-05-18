4a3437ebd29ad04086352ee4861eabe3a3c0cee8
raymart-evangelista <raymart.s.evangelista@gmail.com>
2023-05-18 12:57:07 -0700

    give Card component ability to abstract

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,20 +1,59 @@
-import * as React from 'react'
+import { useState } from 'react'
 
 import { Avatar, Button, Card as PaperCard, Text } from 'react-native-paper'
 
 const LeftComponent = props => <Avatar.Icon {...props} icon="folder" />
 
-const Card = ({title="Default Title", subtitle=null, clickAction=null, notes=null}) => {
+const Card = ({title="Default Title", subtitle=null, clickAction=null, exercise=null}) => {
+
+  // const [content, setContent] = useState(null)
+  // let content
+
+  // if (exercise) {
+  //   content = (
+  //     <PaperCard>
+  //       <PaperCard.Title title={exercise.name}/>
+  //       <PaperCard.Content>
+  //         <Text variant='bodySmall'>{exercise.notes}</Text>
+  //       </PaperCard.Content>
+  //     </PaperCard>
+  //   )
+  // } else {
+  //   content = (
+  //     <PaperCard>
+  //       <PaperCard.Title title={title} subtitle={subtitle} />
+  //       <PaperCard.Content>
+  //         {/* <Text variant="titleLarge">Card title</Text> */}
+  //         {/* <Text variant='bodySmall'>{exercise.notes}</Text> */}
+  //       </PaperCard.Content>
+  //       {/* <PaperCard.Cover source={{ uri: 'https://picsum.photos/700' }} /> */}
+  //       <PaperCard.Actions>
+  //         {/* <Button>Cancel</Button> */}
+  //         {/* <Button>Ok</Button> */}
+  //       </PaperCard.Actions>
+  //     </PaperCard>
+  //   )
+  // }
+
   return (
     <PaperCard>
       {/* <PaperCard.Title title="Card Title" subtitle="Card Subtitle" left={LeftComponent} /> */}
-      <PaperCard.Title title={title} subtitle={subtitle} />
-      <PaperCard.Content>
-        {/* <Text variant="titleLarge">Card title</Text> */}
-        {notes && (
-          <Text variant='bodySmall'>{notes}</Text>
-        )}
-      </PaperCard.Content>
+      {exercise ? (
+          <>
+            <PaperCard.Title title={exercise.name}/>
+            <PaperCard.Content>
+              <Text variant='bodySmall'>{exercise.notes}</Text>
+            </PaperCard.Content>
+          </>
+        ) : (
+          <>
+            <PaperCard.Title title={title} subtitle={subtitle} />
+            <PaperCard.Content>
+              {/* <Text variant="titleLarge">Card title</Text> */}
+              {/* <Text variant='bodySmall'>{exercise.notes}</Text> */}
+            </PaperCard.Content>
+          </>
+      )}
       {/* <PaperCard.Cover source={{ uri: 'https://picsum.photos/700' }} /> */}
       <PaperCard.Actions>
         {/* <Button>Cancel</Button> */}
