be58275e0121923cfec5936badcb2f5116bc03bd
raymart-evangelista <raymart.s.evangelista@gmail.com>
2023-07-01 23:55:18 -0700

    add icons

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -11,14 +11,17 @@ export default function HomeScreen({ navigation, route }) {
       <Button 
         mode='elevated'
         onPress={() => navigation.navigate('CreateProgram')}
+        icon='plus-box-multiple-outline'
       >Create program</Button>
       <Button 
         mode='elevated'
         onPress={() => navigation.navigate('ExistingPrograms')}
+        icon='checkbox-multiple-blank-outline'
       >All programs</Button>
       <Button
         mode='elevated'
         onPress={() => navigation.navigate('Settings')}
+        icon='cog-outline'
       >Settings</Button>
     </View>
   )
