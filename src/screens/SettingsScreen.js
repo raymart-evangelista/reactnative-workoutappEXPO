e3b39edb47956b1f48f629d7390f9345b7866ceb
raymart-evangelista <raymart.s.evangelista@gmail.com>
2023-07-04 17:45:21 -0700

    add switch

--- src/screens/SettingsScreen.js
+++ src/screens/SettingsScreen.js
@@ -1,11 +1,13 @@
 import { Text, View } from 'react-native'
 import { defaultStyles } from '../styles/globalStyles';
+import { Switch } from 'react-native-paper';
 
 export default function SettingsScreen() {
   return (
     <View style={defaultStyles.basic}>
       <Text>Settings!</Text>
       {/* button for toggling dark mode */}
+      <Switch></Switch>
       {/* button for logging out */}
       {/* button for connecting apple watch */}
       {/* button for changing units of measurement */}
