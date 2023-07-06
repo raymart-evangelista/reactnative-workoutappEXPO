4f915912506944f36fcac4d732c0c4e9e0fff4ad
raymart-evangelista <raymart.s.evangelista@gmail.com>
2023-07-05 22:22:15 -0700

    setup switch

--- src/screens/SettingsScreen.js
+++ src/screens/SettingsScreen.js
@@ -1,13 +1,21 @@
 import { Text, View } from 'react-native'
 import { defaultStyles } from '../styles/globalStyles';
 import { Switch } from 'react-native-paper';
+import { useState } from 'react';
 
 export default function SettingsScreen() {
+
+  const [isSwitchOn, setIsSwitchOn] = useState(false)
+  const onToggleSwitch = () => setIsSwitchOn(!isSwitchOn)
+
   return (
     <View style={defaultStyles.basic}>
       <Text>Settings!</Text>
       {/* button for toggling dark mode */}
-      <Switch></Switch>
+      <Switch
+        value={isSwitchOn}
+        onValueChange={onToggleSwitch}
+      />
       {/* button for logging out */}
       {/* button for connecting apple watch */}
       {/* button for changing units of measurement */}
