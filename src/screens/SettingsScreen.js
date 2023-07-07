3f7a316d563bf55fa0a61c33a37b36698d80bd82
raymart-evangelista <raymart.s.evangelista@gmail.com>
2023-07-06 23:29:09 -0700

    remove darkmode attempt

--- src/screens/SettingsScreen.js
+++ src/screens/SettingsScreen.js
@@ -1,22 +1,17 @@
 import { Text, View } from 'react-native'
 import { defaultStyles } from '../styles/globalStyles';
-import { Switch, withTheme } from 'react-native-paper';
+import { Switch } from 'react-native-paper';
 import { useState } from 'react';
 
-export default function SettingsScreen({ theme }) {
-
-  // const { colors } = theme
-  const [isSwitchOn, setIsSwitchOn] = useState(false)
-  const onToggleSwitch = () => setIsSwitchOn(!isSwitchOn)
+export default function SettingsScreen() {
 
   return (
     // <View style={{ backgroundColor: colors.backgroundColor }}>
     <View style={defaultStyles.basic}>
       <Text>Settings!</Text>
       {/* button for toggling dark mode */}
+      {/* use TailwindCSS */}
       <Switch
-        value={isSwitchOn}
-        onValueChange={onToggleSwitch}
       />
       {/* button for logging out */}
       {/* button for connecting apple watch */}
