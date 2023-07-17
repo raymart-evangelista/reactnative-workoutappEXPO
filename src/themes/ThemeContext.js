744a1b9e59caa425874ad06984d79388e1681b68
raymart-evangelista <raymart.s.evangelista@gmail.com>
2023-07-16 21:38:02 -0700

    set up ThemeContext export

--- src/themes/ThemeContext.js
+++ src/themes/ThemeContext.js
@@ -1,5 +1,8 @@
-import { useCallback, useContext, useMemo, useState } from "react";
+import { createContext, useCallback, useContext, useMemo, useState } from "react";
 import { useColorScheme } from "react-native";
+
+import { lightTheme2, darkTheme2 } from "./theme";
+
 import {
   Provider as PaperProvider,
   DarkTheme as PaperDarkTheme,
@@ -7,10 +10,65 @@ import {
 } from 'react-native-paper'
 import {
   NavigationContainer,
+  Theme as NavigationTheme,
   DarkTheme as NavigationDarkTheme,
   DefaultTheme as NavigationDefaultTheme,
 } from '@react-navigation/native'
 
-export default function ThemeContext() {
-  
+const lightTheme = {
+  ...NavigationDefaultTheme,
+  ...PaperDefaultTheme,
+  colors: {
+    ...NavigationDefaultTheme.colors,
+    ...PaperDefaultTheme.colors,
+  }
+}
+
+const darkTheme = {
+  ...NavigationDarkTheme,
+  ...PaperDarkTheme,
+  colors: {
+    ...NavigationDarkTheme.colors,
+    ...PaperDarkTheme.colors,
+  }
+}
+
+export const ThemeContext = createContext({
+  theme: lightTheme,
+  themeType: 'light',
+  isDarkTheme: false,
+  setThemeType: () => {},
+  toggleThemeType: () => {},
+})
+
+export const useTheme = () => useContext(ThemeContext)
+
+export const ThemeContextProvider = ({ children }) => {
+  const colorScheme = useColorScheme()
+  const [themeType, setThemeType] = useState(colorScheme === 'dark' ? 'dark' : 'light')
+
+  const toggleThemeType = useCallback(() => {
+    setThemeType(prev => (prev === 'dark' ? 'light' : 'dark'))
+  }, [])
+
+  const isDarkTheme = useMemo(() => themeType === 'dark', [themeType])
+  const theme = useMemo(() => (isDarkTheme ? darkTheme : lightTheme), [isDarkTheme])
+
+  return (
+    <ThemeContext.Provider
+      value={{
+        theme,
+        themeType,
+        isDarkTheme,
+        setThemeType,
+        toggleThemeType,
+      }}
+    >
+      <NavigationContainer theme={theme}>
+        <PaperProvider theme={theme}>
+          {children}
+        </PaperProvider>
+      </NavigationContainer>
+    </ThemeContext.Provider>
+  )
 }
