592c59a304eec8039d74b627f449cd1e894cfa38
raymart-evangelista <raymart.s.evangelista@gmail.com>
2023-08-01 00:15:36 -0700

    fix dark mode Button

--- src/screens/SettingsScreen.js
+++ src/screens/SettingsScreen.js
@@ -11,14 +11,28 @@ export default function SettingsScreen() {
   const { toggleThemeType, themeType, isDarkTheme, theme } = useTheme()
   const getOppositeTheme = () => (themeType === 'light' ? 'dark' : 'light');
 
+  const iconType = theme.dark ? 'white-balance-sunny' : 'moon-waxing-crescent'
 
   return (
     <View className="flex-1 justify-center items-center">
       {/* <Text>Settings!</Text> */}
      <View>
-       <Button className="w-32" mode="contained" onPress={toggleThemeType}>
-        {`${getOppositeTheme()} mode`}
+       <Button
+        className="w-min" 
+        mode="contained" 
+        onPress={toggleThemeType}
+        icon={iconType}
+        contentStyle={{flexDirection: 'row-reverse'}}
+      >
+        {`${getOppositeTheme()}`}
        </Button>
+       {/* <Button
+          mode="contained"
+          icon={iconType}
+          contentStyle={{flexDirection: 'row-reverse'}}
+        >
+          Next
+        </Button> */}
        {/* <Headline>{themeType}</Headline> */}
        {/* <Headline>isDarkTheme: {`${isDarkTheme}`}</Headline> */}
        {/* <Headline>Primary: {theme.colors.primary}</Headline> */}
