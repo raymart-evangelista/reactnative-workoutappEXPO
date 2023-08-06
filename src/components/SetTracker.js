5d0b6230553cdaac7cfe2af7a17340e80e488e14
raymart-evangelista <raymart.s.evangelista@gmail.com>
2023-08-05 23:23:13 -0700

    fix completedSets implementation in SetTracker

--- src/components/SetTracker.js
+++ src/components/SetTracker.js
@@ -3,36 +3,41 @@ import { View } from "react-native";
 import { Text, Button, RadioButton } from "react-native-paper";
 
 const SetTracker = ({ sets, type, onSetCompletion }) => {
-  // const [completedSets, setCompletedSets] = useState(new Array(parseInt(sets)).fill(false))
+
+  // Local state to manage which radio buttons are checked
+  const [completedSets, setCompletedSets] = useState(new Array(parseInt(sets)).fill(false))
   // console.log(completedSets)
 
-  const [setsCompletion, setSetsCompletion] = useState(new Array(sets).fill(false))
+  // const [setsCompletion, setSetsCompletion] = useState(new Array(sets).fill(false))
 
-  const handleSetCompletion = (setIndex, completionStatus) => {
-    const updatedCompletion = [...setsCompletion]
-    updatedCompletion[setIndex] = completionStatus
-    setSetsCompletion(updatedCompletion)
+  // const handleSetCompletion = (setIndex, completionStatus) => {
+  //   const updatedCompletion = [...setsCompletion]
+  //   updatedCompletion[setIndex] = completionStatus
+  //   setSetsCompletion(updatedCompletion)
 
-    onSetCompletion(type, setIndex, completionStatus)
-  }
+  //   onSetCompletion(type, setIndex, completionStatus)
+  // }
 
   const handleRadioPress = (index) => {
     const updatedCompletedSets = [...completedSets]
-    updatedCompletedSets[index] = !updatedCompletedSets[index]
+    updatedCompletedSets[index] = !updatedCompletedSets[index] // toggle completion state (check/uncheck)
     setCompletedSets(updatedCompletedSets)
+
+    // Callback to parent component
+    onSetCompletion(type, index, updatedCompletion[index])
   }
 
   return (
     <View>
       <Text>{`Completed ${completedSets.filter(Boolean).length} of ${sets} ${type} sets`}</Text>
       {/* {completedSets.map((completed, index) => ( */}
-      { setsCompletion.map((completed, index) => (
+      { completedSets.map((isSetCompleted, index) => (
         <View key={index} style={{ flexDirection: 'row', alignItems: 'center' }}>
           <RadioButton.IOS
             value={`set-${index}`}
             mode='ios'
-            status={completed ? "checked" : "unchecked"}
-            onPress={() => handleSetCompletion(index, !completed)} // Toggle completion status
+            status={isSetCompleted ? "checked" : "unchecked"}
+            onPress={() => handleRadioPress(index)} // Toggle completion status
           />
           <Text>{`Set ${index + 1}`}</Text>
         </View>
