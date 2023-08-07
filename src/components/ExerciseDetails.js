4ec8ed939ec7f4aa04541e37bb1f263bf6db3540
raymart-evangelista <raymart.s.evangelista@gmail.com>
2023-08-06 22:32:35 -0700

    create new component

---
+++ src/components/ExerciseDetails.js
@@ -0,0 +1,10 @@
+import { Text, TextInput } from 'react-native-paper'
+import { View } from 'react-native'
+import SetTracker from './SetTracker'
+
+const ExerciseDetails = ({ exercise, weightValue, handleWeightChange }) => {
+  return (
+    <>
+    </>
+  )
+}
