c184244befde83e26126f3e57ca92b6b95cefa91
raymart-evangelista <raymart.s.evangelista@gmail.com>
2023-08-07 22:22:14 -0700

    add code

--- src/components/ExerciseDetails.js
+++ src/components/ExerciseDetails.js
@@ -5,6 +5,33 @@ import SetTracker from './SetTracker'
 const ExerciseDetails = ({ exercise, weightValue, handleWeightChange }) => {
   return (
     <>
+      <Text variant='bodySmall'>{exercise.notes}</Text>
+      {exercise.warmupSets.min === exercise.warmupSets.max ? (
+        <View>
+          <Text variant='bodyMedium'>warm up sets: {exercise.warmupSets.min}</Text>
+        </View>
+      ) : (
+        <View>
+          <Text variant='bodyMedium'>warm up sets: {exercise.warmupSets.min}-{exercise.warmupSets.max}</Text>
+          <SetTracker 
+            sets={exercise.warmupSets.max} 
+            type="warmup"
+          />
+        </View>
+      )}
+      {exercise.workingSets.min === exercise.workingSets.max ? (
+        <View>
+          <Text variant='bodyMedium'>working sets: {exercise.workingSets.min} x {exercise.reps.min}-{exercise.reps.max} reps {exercise.reps.notes && ` (${exercise.reps.notes})`}</Text>
+        </View>
+      ) : (
+        <View>
+          <Text variant='bodyMedium'>working sets: {exercise.workingSets.min}-{exercise.workingSets.max} x {exercise.reps.min}-{exercise.reps.max} reps ({exercise.reps.notes})</Text>
+          <SetTracker
+            sets={exercise.workingSets.max}
+            type="working"
+          />
+        </View>
+      )}
     </>
   )
 }
