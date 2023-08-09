a4a7137d9263cbf37d8b1f1b2d91c50c230da268
raymart-evangelista <raymart.s.evangelista@gmail.com>
2023-08-08 23:57:07 -0700

    add useExerciseWeight hook code

--- src/hooks/useExerciseWeight.js
+++ src/hooks/useExerciseWeight.js
@@ -1,8 +1,24 @@
 import { useState, useEffect } from 'react'
-import programs from '../services/programs'
+import programsService from "../services/programs"
 
-const useExerciseWeight = () => {
+const useExerciseWeight = (initialWeight, program, week, day, exercise) => {
+  const [weightValue, setWeightValue] = useState(initialWeight)
 
+  useEffect(() => {
+    const updateWeightValue = async () => {
+      try {
+        if (exercise && exercise.weight.value !== weightValue) {
+          await programsService.updateExerciseWeight(program.id, week._id, day._id, exercise._id, weightValue)
+        }
+      } catch (error) {
+        console.error('Failed to update exercise weight: ', error)
+      }
+    }
+
+    updateWeightValue()
+  }, [weightValue])
+
+  return [weightValue, setWeightValue]
 }
 
 export default useExerciseWeight
