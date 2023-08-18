d3a59302d64ea045d56dd620b7e1e410a58dca17
raymart-evangelista <raymart.s.evangelista@gmail.com>
2023-08-18 11:15:35 -0700

    update computeCompletionFields such that it works with POST and PATCH method

--- server/middleware/computeCompletionFields.js
+++ server/middleware/computeCompletionFields.js
@@ -3,14 +3,11 @@ const Program = require('../models/program')
 
 module.exports = async (req, res, next) => {
   const { weekDetails } = req.body
-  const currentProgram = await Program.findById(req.params.id)
 
-  weekDetails.forEach((week, weekIndex) => {
-    week.dayDetails.forEach((day, dayIndex) => {
-      day.exercises.forEach((exercise, exerciseIndex) => {
-        const currentExercise = currentProgram.weekDetails[weekIndex].dayDetails[dayIndex].exercises[exerciseIndex]
-
-        if (compareExercises(currentExercise, exercise)) {
+  if (req.method === 'POST') {
+    weekDetails.forEach(week => {
+      week.dayDetails.forEach(day => {
+        day.exercises.forEach(exercise => {
           exercise.warmupSetsCompletion = {
             individual: new Array(Number(exercise.warmupSets.max)).fill(false),
             overall: false
@@ -19,9 +16,32 @@ module.exports = async (req, res, next) => {
             individual: new Array(Number(exercise.workingSets.max)).fill(false),
             overall: false
           }
-        }
+        })
       })
     })
-  });
-  next()
+  }
+
+  if (req.method === 'PATCH') {
+    const currentProgram = await Program.findById(req.params.id)
+  
+    weekDetails.forEach((week, weekIndex) => {
+      week.dayDetails.forEach((day, dayIndex) => {
+        day.exercises.forEach((exercise, exerciseIndex) => {
+          const currentExercise = currentProgram.weekDetails[weekIndex].dayDetails[dayIndex].exercises[exerciseIndex]
+  
+          if (compareExercises(currentExercise, exercise)) {
+            exercise.warmupSetsCompletion = {
+              individual: new Array(Number(exercise.warmupSets.max)).fill(false),
+              overall: false
+            }
+            exercise.workingSetsCompletion = {
+              individual: new Array(Number(exercise.workingSets.max)).fill(false),
+              overall: false
+            }
+          }
+        })
+      })
+    });
+    next()
+  }
 }
