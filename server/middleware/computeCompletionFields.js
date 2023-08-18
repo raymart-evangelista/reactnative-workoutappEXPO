8d876e66682a627d54905dc4915986d153841de6
raymart-evangelista <raymart.s.evangelista@gmail.com>
2023-08-17 23:51:32 -0700

    refactor completion field updates to target specific exercises
    now only updates completion fields for exercises where warmup or working sets have been modified, preserving existing completion states for other exercises

--- server/middleware/computeCompletionFields.js
+++ server/middleware/computeCompletionFields.js
@@ -1,21 +1,27 @@
-module.exports = (req, res, next) => {
-  const { weekDetails } = req.body
-  // console.log(weekDetails)
+const { programIsDifferent } = require('../utils/programUtils')
+const Program = require('../models/program')
 
-  weekDetails.forEach(week => {
-    week.dayDetails.forEach(day => {
-      day.exercises.forEach(exercise => {
-        exercise.warmupSetsCompletion = {
-          individual: new Array(Number(exercise.warmupSets.max)).fill(false),
-          overall: false
-        }
-        exercise.workingSetsCompletion = {
-          individual: new Array(Number(exercise.workingSets.max)).fill(false),
-          overall: false
+module.exports = async (req, res, next) => {
+  const { weekDetails } = req.body
+  const currentProgram = await Program.findById(req.params.id)
+
+  weekDetails.forEach((week, weekIndex) => {
+    week.dayDetails.forEach((day, dayIndex) => {
+      day.exercises.forEach((exercise, exerciseIndex) => {
+        const currentExercise = currentProgram.weekDetails[weekIndex].dayDetails[dayIndex].exercises[exerciseIndex]
+
+        if (compareExercises(currentExercise, exercise)) {
+          exercise.warmupSetsCompletion = {
+            individual: new Array(Number(exercise.warmupSets.max)).fill(false),
+            overall: false
+          }
+          exercise.workingSetsCompletion = {
+            individual: new Array(Number(exercise.workingSets.max)).fill(false),
+            overall: false
+          }
         }
       })
     })
   });
-
   next()
 }
