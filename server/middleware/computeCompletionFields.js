cd530a1f9a37cc820aed5ebce7d4fed22d9707ce
raymart-evangelista <raymart.s.evangelista@gmail.com>
2023-08-23 13:24:14 -0700

    remove PATCH code

--- server/middleware/computeCompletionFields.js
+++ server/middleware/computeCompletionFields.js
@@ -20,30 +20,5 @@ module.exports = async (req, res, next) => {
       })
     })
   }
-
-  // if (req.method === 'PATCH') {
-  //   const currentProgram = await Program.findById(req.params.id)
-  //   weekDetails.forEach((week, weekIndex) => {
-  //     week.dayDetails.forEach((day, dayIndex) => {
-  //       day.exercises.forEach((exercise, exerciseIndex) => {
-  //         const currentExercise = currentProgram.weekDetails[weekIndex].dayDetails[dayIndex].exercises[exerciseIndex]
-  //         const updatedExercise = weekDetails[weekIndex].dayDetails[dayIndex].exercises[exerciseIndex]
-
-  //         if (currentExercise && updatedExercise) {
-  //           console.log('this is the exercise warmup sets max: ')
-  //           console.log(exercise.warmupSets.max)
-  //           exercise.warmupSetsCompletion = {
-  //             individual: new Array(Number(exercise.warmupSets.max)).fill(false),
-  //             overall: false
-  //           }
-  //           // exercise.workingSetsCompletion = {
-  //           //   individual: new Array(Number(exercise.workingSets.max)).fill(false),
-  //           //   overall: false
-  //           // }
-  //         }
-  //       })
-  //     })
-  //   })
-  // }
   next()
 }
