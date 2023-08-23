c045db54790693c258e8ead575180b751dce3020
raymart-evangelista <raymart.s.evangelista@gmail.com>
2023-08-22 23:15:29 -0700

    remove PATCH method since that wouldn't be a middleware function

--- server/middleware/computeCompletionFields.js
+++ server/middleware/computeCompletionFields.js
@@ -21,50 +21,29 @@ module.exports = async (req, res, next) => {
     })
   }
 
-  if (req.method === 'PATCH') {
-    const currentProgram = await Program.findById(req.params.id)
+  // if (req.method === 'PATCH') {
+  //   const currentProgram = await Program.findById(req.params.id)
+  //   weekDetails.forEach((week, weekIndex) => {
+  //     week.dayDetails.forEach((day, dayIndex) => {
+  //       day.exercises.forEach((exercise, exerciseIndex) => {
+  //         const currentExercise = currentProgram.weekDetails[weekIndex].dayDetails[dayIndex].exercises[exerciseIndex]
+  //         const updatedExercise = weekDetails[weekIndex].dayDetails[dayIndex].exercises[exerciseIndex]
 
-    const isDifferent = programIsDifferent(currentProgram, weekDetails)
-
-    if (isDifferent) {
-      weekDetails.forEach((week, weekIndex) => {
-        week.dayDetails.forEach((day, dayIndex) => {
-          day.exercises.forEach((exercise, exerciseIndex) => {
-            const currentExercise = currentProgram.weekDetails[weekIndex].dayDetails[dayIndex].exercises[exerciseIndex]
-            const updatedExercise = weekDetails[weekIndex].dayDetails[dayIndex].exercises[exerciseIndex]
-
-            if (currentExercise && updatedExercise) {
-              exercise.warmupSetsCompletion = {
-                individual: new Array(Number(exercise.warmupSets.max)).fill(false),
-                overall: false
-              }
-              exercise.workingSetsCompletion = {
-                individual: new Array(Number(exercise.workingSets.max)).fill(false),
-                overall: false
-              }
-            }
-          })
-        })
-      })
-    }
-    // weekDetails.forEach((week, weekIndex) => {
-    //   week.dayDetails.forEach((day, dayIndex) => {
-    //     day.exercises.forEach((exercise, exerciseIndex) => {
-    //       const currentExercise = currentProgram.weekDetails[weekIndex].dayDetails[dayIndex].exercises[exerciseIndex]
-  
-    //       if (programIsDifferent(currentExercise, exercise)) {
-    //         exercise.warmupSetsCompletion = {
-    //           individual: new Array(Number(exercise.warmupSets.max)).fill(false),
-    //           overall: false
-    //         }
-    //         exercise.workingSetsCompletion = {
-    //           individual: new Array(Number(exercise.workingSets.max)).fill(false),
-    //           overall: false
-    //         }
-    //       }
-    //     })
-    //   })
-    // });
-  }
+  //         if (currentExercise && updatedExercise) {
+  //           console.log('this is the exercise warmup sets max: ')
+  //           console.log(exercise.warmupSets.max)
+  //           exercise.warmupSetsCompletion = {
+  //             individual: new Array(Number(exercise.warmupSets.max)).fill(false),
+  //             overall: false
+  //           }
+  //           // exercise.workingSetsCompletion = {
+  //           //   individual: new Array(Number(exercise.workingSets.max)).fill(false),
+  //           //   overall: false
+  //           // }
+  //         }
+  //       })
+  //     })
+  //   })
+  // }
   next()
 }
