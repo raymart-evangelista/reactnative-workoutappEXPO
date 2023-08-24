70c491c3e5c4a628cd70cfbf4daacf151a4c53b6
raymart-evangelista <raymart.s.evangelista@gmail.com>
2023-08-23 22:38:43 -0700

    remove unneeded code

--- server/utils/programUtils.js
+++ server/utils/programUtils.js
@@ -10,31 +10,6 @@ const compareExercises = (currentExercise, updatedExercise, setType) => {
   return false;
 };
 
-// const compareDays = (currentDay, updatedDay) => {
-//   for (let i = 0; i < currentDay.exercises.length; i++) {
-//     if (compareExercises(currentDay.exercises[i], updatedDay.exercises[i])) return true;
-//   }
-//   return false;
-// };
-
-// const programIsDifferent = (currentProgram, updatedProgram) => {
-//   for (let weekIndex = 0; weekIndex < currentProgram.weekDetails.length; weekIndex++) {
-//     const week = currentProgram.weekDetails[weekIndex]
-//     for (let dayIndex = 0; dayIndex < week.dayDetails.length; dayIndex++) {
-//       const day = week.dayDetails[dayIndex]
-//       for (let exerciseIndex = 0; exerciseIndex < day.exercises.length; exerciseIndex++) {
-//         const currentExercise = currentProgram.weekDetails[weekIndex].dayDetails[dayIndex].exercises[exerciseIndex]
-//         const updatedExercise = updatedProgram.weekDetails[weekIndex].dayDetails[dayIndex].exercises[exerciseIndex]
-//         if (compareExercises(currentExercise, updatedExercise)) {
-//           console.log('exercise warmup sets were different')
-//           return true
-//         }
-//       }
-//     }
-//   }
-//   return false
-// }
-
 const setsAreDifferent = (currentProgram, updatedProgram, setType) => {
   for (let weekIndex = 0; weekIndex < currentProgram.weekDetails.length; weekIndex++) {
     const week = currentProgram.weekDetails[weekIndex]
