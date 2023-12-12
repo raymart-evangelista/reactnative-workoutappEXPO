ff586ce7467471775b937da2497e33332bf8b165
raymart-evangelista <raymart.s.evangelista@gmail.com>
2023-12-11 23:17:52 -0800

    add AuthContext

--- App.js
+++ App.js
@@ -22,28 +22,34 @@ import LandingPage from './src/screens/LandingPage';
 const Stack = createNativeStackNavigator()
 const Tab = createBottomTabNavigator()
 
+import { useContext } from 'react';
+import { AuthContext } from './src/contexts/AuthContext';
 
 export default function App() {
 
+  // const auth = useContext(AuthContext)
+
   return (
-    <ThemeContextProvider>
-      <Stack.Navigator initialRouteName='Landing' screenOptions={{headerShown: false}}>
-        <Stack.Screen name="Landing" component={LandingPage} />
-        <Stack.Screen name="LogIn" component={LogInScreen} />
-        <Stack.Screen name="SignUp" component={SignUpScreen} />
-        <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="CreateProgram" component={CreateProgramScreen} />
-        <Stack.Screen name="ExistingPrograms" component={ExistingProgramsScreen} />
-        <Stack.Screen name="EditProgram" component={EditProgramScreen} />
-        {/* <Stack.Screen name="ContinueProgram" component={ContinueProgramScreen} /> */}
-        <Stack.Screen name="ProgramInformation" component={ProgramInformationScreen} />
-        <Stack.Screen name="ContinueWeek" component={ContinueWeekScreen} />
-        <Stack.Screen name="ContinueDay" component={ContinueDayScreen} />
-        <Stack.Screen name="Settings" component={SettingsScreen} />
-      </Stack.Navigator>
-      {/* <Stack.Navigator>
-        <Stack.Screen name="Test" component={TestScreen} />
-      </Stack.Navigator> */}
-    </ThemeContextProvider>
+    <AuthContext.Provider>
+      <ThemeContextProvider>
+        <Stack.Navigator initialRouteName='Landing' screenOptions={{headerShown: false}}>
+          <Stack.Screen name="Landing" component={LandingPage} />
+          <Stack.Screen name="LogIn" component={LogInScreen} />
+          <Stack.Screen name="SignUp" component={SignUpScreen} />
+          <Stack.Screen name="Home" component={HomeScreen} />
+          <Stack.Screen name="CreateProgram" component={CreateProgramScreen} />
+          <Stack.Screen name="ExistingPrograms" component={ExistingProgramsScreen} />
+          <Stack.Screen name="EditProgram" component={EditProgramScreen} />
+          {/* <Stack.Screen name="ContinueProgram" component={ContinueProgramScreen} /> */}
+          <Stack.Screen name="ProgramInformation" component={ProgramInformationScreen} />
+          <Stack.Screen name="ContinueWeek" component={ContinueWeekScreen} />
+          <Stack.Screen name="ContinueDay" component={ContinueDayScreen} />
+          <Stack.Screen name="Settings" component={SettingsScreen} />
+        </Stack.Navigator>
+        {/* <Stack.Navigator>
+          <Stack.Screen name="Test" component={TestScreen} />
+        </Stack.Navigator> */}
+      </ThemeContextProvider>
+    </AuthContext.Provider>
   );
 }
