023a4e74443fbcdb26093a553c5eb3ca67f927f8
raymart-evangelista <raymart.s.evangelista@gmail.com>
2023-12-14 00:50:19 -0800

    add AuthProvider to AuthContext

--- src/contexts/AuthContext.js
+++ src/contexts/AuthContext.js
@@ -1,8 +1,23 @@
-import { createContext } from "react";
+import { useState, createContext } from "react";
 
 export const AuthContext = createContext({
   user: null,
-  login: () => {},
-  logout: () => {},
-  isAuthenticated: false,
+  login: (userData) => {setUser(userData)},
+  logout: () => {setUser(null)},
 })
+
+const AuthProvider = ({ children }) => {
+  const [authState, setAuthState] = useState({
+    user: null,
+    login: (userData) => {setUser(userData)},
+    logout: () => {setUser(null)},
+  })
+
+  return (
+    <AuthContext.Provider value={[authState, setAuthState]}>
+      {children}
+    </AuthContext.Provider>
+  )
+}
+
+export default AuthProvider
