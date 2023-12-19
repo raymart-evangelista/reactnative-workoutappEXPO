b3053047c11279efe00f3a4cc7f15d18943a818b
raymart-evangelista <raymart.s.evangelista@gmail.com>
2023-12-19 13:30:04 -0800

    seperation of concerns--move gesture stuff to own function

--- src/screens/CreateProgramScreen.js
+++ src/screens/CreateProgramScreen.js
@@ -20,21 +20,7 @@ import { GestureHandlerRootView, Gesture, GestureDetector } from "react-native-g
 
 const AnimatedCircle = Animated.createAnimatedComponent(Circle)
 
-const CreateProgramScreen = () => {
-    const { control, handleSubmit, formState: { errors } } = useForm({
-        defaultValues: {
-            programName: "",
-            weeks: []
-        }
-    })
-
-    const onSubmit = (data) => console.log(data)
-
-    const { fields: weekFields, append: appendWeek } = useFieldArray({
-        control,
-        name: "weeks"
-    })
-
+const GestureScreen = () => {
     const width = useSharedValue(100)
     const handlePress = () => {
         width.value = withSpring(width.value + 50)
@@ -105,64 +91,85 @@ const CreateProgramScreen = () => {
             offset.value = withSpring(0)
             pressed.value = false
         })
-    return (
-        // <SafeAreaView>
-        //     <Controller
-        //         control={control}
-        //         rules={{ required: true }}
-        //         render={({ field: { onChange, onBlur, value } }) => (
-        //             <TextInput
-        //                 placeholder="Program Name"
-        //                 onBlur={onBlur}
-        //                 onChangeText={onChange}
-        //                 value={value}
-        //             />
-        //         )}
-        //         name="programName"
-        //     />
-        //     {errors.programName && <Text>Program name is needed.</Text>}
-        //     {/* button to add weeks */}
-        //     <Button onPress={() => appendWeek({ days: []})}>Add Week</Button>
-        //     {weekFields.map((week, weekIndex) => (
-        //         <TouchableOpacity key={week.id} onPress={() => {/* navigate to day screen */}}>
-        //         <Text>Week {weekIndex + 1}</Text>
-        //         </TouchableOpacity>
-        //         ))}
-        //     <Button onPress={handleSubmit(onSubmit)}>Submit</Button>
-        // </SafeAreaView>
-        // <SafeAreaView style={{ flex: 1, alignItems: 'center' }}>
-        //     <Animated.View
-        //         style={{
-        //             width,
-        //             height: 100,
-        //             backgroundColor: "red",
-        //         }}
-        //     />
-        //     <Button onPress={handlePress}>Click me</Button>
-        //     <Button onPress={handlePress2}>Click me</Button>
-        //     <Animated.View style={[styles.box, animatedStyles]} />
-        //     <View style={styles.container}>
-        //         <Button onPress={handlePress3}>Click me</Button>
-        //         <Button onPress={handlePress4}>Click me</Button>
-        //     </View>
-        // </SafeAreaView>
 
-            // <View style={styles.container}>
-            //     <Svg style={styles.svg}>
-            //         <AnimatedCircle cx="50%" cy="50%" fill="#b58df1" animatedProps={animatedProps} />
-            //     </Svg>
-            //     <Button onPress={handlePress5}>Click 5</Button>
-            //     <Button onPress={handlePress6}>Click 6</Button>
-            // </View>
-    
+    return (
         <GestureHandlerRootView style={styles.container}>
-            <View style={styles.container}>
-                <GestureDetector gesture={pan}>
-                    <Animated.View style={[styles.circle, animatedGestureStyles]} />
-                </GestureDetector>
-            </View>
-            
-        </GestureHandlerRootView>
+        <View style={styles.container}>
+            <GestureDetector gesture={pan}>
+                <Animated.View style={[styles.circle, animatedGestureStyles]} />
+            </GestureDetector>
+        </View>
+        
+        {/* <View style={styles.container}>
+        <Svg style={styles.svg}>
+            <AnimatedCircle cx="50%" cy="50%" fill="#b58df1" animatedProps={animatedProps} />
+        </Svg>
+        <Button onPress={handlePress5}>Click 5</Button>
+        <Button onPress={handlePress6}>Click 6</Button>
+        </View> */}
+        {/* <SafeAreaView style={{ flex: 1, alignItems: 'center' }}>
+        <Animated.View
+            style={{
+                width,
+                height: 100,
+                backgroundColor: "red",
+            }}
+        />
+        <Button onPress={handlePress}>Click me</Button>
+        <Button onPress={handlePress2}>Click me</Button>
+        <Animated.View style={[styles.box, animatedStyles]} />
+        <View style={styles.container}>
+            <Button onPress={handlePress3}>Click me</Button>
+            <Button onPress={handlePress4}>Click me</Button>
+        </View>
+        </SafeAreaView> */}
+    </GestureHandlerRootView>
+
+
+    )
+}
+
+const CreateProgramScreen = () => {
+    const { control, handleSubmit, formState: { errors } } = useForm({
+        defaultValues: {
+            programName: "",
+            weeks: []
+        }
+    })
+
+    const onSubmit = (data) => console.log(data)
+
+    const { fields: weekFields, append: appendWeek } = useFieldArray({
+        control,
+        name: "weeks"
+    })
+
+ 
+    return (
+        <SafeAreaView>
+            <Controller
+                control={control}
+                rules={{ required: true }}
+                render={({ field: { onChange, onBlur, value } }) => (
+                    <TextInput
+                        placeholder="Program Name"
+                        onBlur={onBlur}
+                        onChangeText={onChange}
+                        value={value}
+                    />
+                )}
+                name="programName"
+            />
+            {errors.programName && <Text>Program name is needed.</Text>}
+            {/* button to add weeks */}
+            <Button onPress={() => appendWeek({ days: []})}>Add Week</Button>
+            {weekFields.map((week, weekIndex) => (
+                <TouchableOpacity key={week.id} onPress={() => {/* navigate to day screen */}}>
+                <Text>Week {weekIndex + 1}</Text>
+                </TouchableOpacity>
+                ))}
+            <Button onPress={handleSubmit(onSubmit)}>Submit</Button>
+        </SafeAreaView>
     )
 }
 
