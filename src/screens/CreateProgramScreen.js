db79f36099e1f558c249ab336737cf5b42c11295
raymart-evangelista <raymart.s.evangelista@gmail.com>
2024-01-11 11:40:13 -0800

    add boilerplate for adding day

--- src/screens/CreateProgramScreen.js
+++ src/screens/CreateProgramScreen.js
@@ -18,314 +18,341 @@ import { Circle, Svg } from "react-native-svg";
 import { Gesture, GestureDetector, ScrollView } from "react-native-gesture-handler";
 
 import uuid from 'react-native-uuid'
-import DraggableFlatList, { ScaleDecorator, } from 'react-native-draggable-flatlist'
+import DraggableFlatList, { ScaleDecorator, ShadowDecorator, OpacityDecorator } from 'react-native-draggable-flatlist'
 
 
 const AnimatedBoxHeightValue = 65
 
-const AnimatedBox = ({ index, box, onDelete, onDrag, isActive }) => {
-    // const [isExpanded, setIsExpanded] = useState(false)
-    const [expandedText, setExpandedText] = useState('Expand')
-    const [buttonPressed, setButtonPressed] = useState(false)
-    
-    
-    const pressed = useSharedValue(false)
-    const height = useSharedValue(AnimatedBoxHeightValue)
-    const expand = useSharedValue(false)
-
-    const longPressed = useSharedValue(false)
-
-    const calculateNewBoxPosition = (index) => {
-        return index * 110
-    }
-
-    // const toggleExpand = () => {
-    //     setIsExpanded(!isExpanded)
-    //     setExpandedText(isExpanded ? 'Expand' : 'Close')
-    //     height.value = isExpanded ? AnimatedBoxHeightValue : 200
-    // }
-
-    const tap = Gesture.Tap()
-        .onEnd(() => {
-            console.log(box.index)
-            pressed.value = !pressed.value
-        })
-        
-    // const longPress = Gesture.LongPress().minDuration(1000)
-    //     .onBegin(() => {
-            
-    //     })
-    //     .onStart(() => {
-    //         console.log('after minDuration')
-    //         pressed.value = true
-    //     })
-    //     .onEnd(() => {
-    //         console.log(`long pressed`)
-
-    //     })
-    //     .onFinalize(() => {
-    //         pressed.value = false
-    //     })
-
-    const animatedStyle = useAnimatedStyle(() => {
-        const y = 0
-        return {
-            // height: withTiming(height.value, {
-            //     duration: 200,
-            // }),
-            height: withTiming(pressed.value ? 200 : AnimatedBoxHeightValue, {
-                duration: 200,
-            }),
-            transform: [ 
-                {translateY: y}, 
-                // {scale: withTiming(pressed.value ? 1.1 : 1)}, 
-                // {height: withTiming(pressed.value ? 200 : AnimatedBoxHeightValue )}
-            ],
-        }
-    })
-
-    const animatedContentStyle = useAnimatedStyle(() => {
-        return {
-            opacity: pressed.value ? 1 : 0,
-            height: pressed.value ? 'auto' : 0,
-            paddingVertical: 15,
-        }
-    })
-
-    const handleTest = () => {
-        console.log(box.key)
-    }
-
-    const handleDelete = () => {
-        setButtonPressed(true)
-        onDelete()
-        setTimeout(() => setButtonPressed(false), 300)
-    }
-
-    const handleAdd = () => {
-        setButtonPressed(true)
-        console.log('add')
-        setTimeout(() => setButtonPressed(false), 300)
-    }
-    
-    return (
-        <Animated.View
-            // className="border-2 border-rose-600" 
-            entering={FadeIn} 
-            style={[ styles.weekBox, animatedStyle ]}
-        >
-            <GestureDetector gesture={tap}>
-                <TouchableOpacity
-                    // className="border-2 border-blue-600" 
-                    style={styles.gestureTapArea} 
-                    onLongPress={onDrag} 
-                    disabled={isActive}
-                >
-                        <Text>Week {box.index + 1}</Text>
-                </TouchableOpacity>
-            </GestureDetector>
-
-            <Animated.View style={[ animatedContentStyle ]}>
-                <TouchableOpacity onLongPress={onDrag} disabled={isActive} >
-                    <View style={styles.header}>
-                        {/* <Button onPress={toggleExpand}>{expandedText}</Button> */}
-                    </View>
-                        <Text>Test</Text>
-                        <Button onPress={handleDelete}>Delete</Button>
-                        <Button onPress={handleAdd}>add</Button>
-                </TouchableOpacity>                        
-            </Animated.View>
-        </Animated.View>
-    )
+const generateUniqueId = () => {
+	return uuid.v4()
 }
 
-const CreateProgramScreen = () => {
-    // const { control, handleSubmit, formState: { errors } } = useForm({
-    //     defaultValues: {
-    //         programName: "",
-    //         weeks: []
-    //     }
-    // })
+const AnimatedDayBox = ({ day, onDelete, onDrag, isActive }) => {
+	console.log('inside animated day box')
+}
 
-    const onSubmit = (data) => console.log(data)
+const AnimatedWeekBox = ({ index, box, onDelete, onDrag, isActive }) => {
+	const [days, setDays] = useState(
+		[		
+			{
+				key: generateUniqueId()
+			}
+		]
+	)
 
-    // const { fields: weekFields, append: appendWeek } = useFieldArray({
-    //     control,
-    //     name: "weeks"
-    // })
+	const pressed = useSharedValue(false)
+	const height = useSharedValue(AnimatedBoxHeightValue)
+	const expand = useSharedValue(false)
 
-    // Create an array to hold animated values for each week
-    // const weekAnimations = weekFields.map(() => useSharedValue(0))
+	const longPressed = useSharedValue(false)
 
-    // Function to append a week and initialize its animation
-    // const handleAddWeek = () => {
-    //     appendWeek({ days: [] })
-    //     appendWeek({ days: [] })
-    //     const newWeekAnimation = useSharedValue(0)
-    //     newWeekAnimation.value = withTiming(1, { duration: 500 })
-    //     weekAnimations.push(newWeekAnimation)
-    // }
+	const calculateNewBoxPosition = (index) => {
+		return index * 110
+	}
 
-    const generateUniqueId = () => {
-        return uuid.v4()
-    }
+	// const toggleExpand = () => {
+	//     setIsExpanded(!isExpanded)
+	//     setExpandedText(isExpanded ? 'Expand' : 'Close')
+	//     height.value = isExpanded ? AnimatedBoxHeightValue : 200
+	// }
 
-    const handleRemoveWeek = (boxToRemove) => {
-        const keyToRemove = boxToRemove.key
-        setBoxes((currentBoxes) => currentBoxes.filter((box, index) => box.key !== keyToRemove))
-    }
+	const tap = Gesture.Tap()
+		.onEnd(() => {
+			console.log(box.index)
+			pressed.value = !pressed.value
+		})
 
-    const handleAddWeek = () => {
-        const newBoxIndex = boxes.length
-        setBoxes(prevBoxes => [...prevBoxes, {
-            key: generateUniqueId(),
-            index: newBoxIndex,
-        }])
-        console.log(boxes)
-    }
+	// const longPress = Gesture.LongPress().minDuration(1000)
+	//     .onBegin(() => {
 
-    const calculateNewBoxPosition = (index) => {
-        return index * 110
-    }
+	//     })
+	//     .onStart(() => {
+	//         console.log('after minDuration')
+	//         pressed.value = true
+	//     })
+	//     .onEnd(() => {
+	//         console.log(`long pressed`)
 
-    const [boxes, setBoxes] = useState(
-        [
-            {
-                key: generateUniqueId(),
-                index: 0,
-            }
-        ]
-    )
- 
-    const renderItem = ({ item, drag, isActive}) => {
+	//     })
+	//     .onFinalize(() => {
+	//         pressed.value = false
+	//     })
 
-        return (
-            <ScaleDecorator>
-                <AnimatedBox
-                    box={item}
-                    index={item.index}
-                    onDelete={() => handleRemoveWeek(item)}
-                    onDrag={drag}
-                    isActive={isActive}
-                />
-            </ScaleDecorator>
-        )
-    }
+	const animatedStyle = useAnimatedStyle(() => {
+		const y = 0
+		return {
+			// height: withTiming(height.value, {
+			//     duration: 200,
+			// }),
+			height: withTiming(pressed.value ? 200 : AnimatedBoxHeightValue, {
+				duration: 200,
+			}),
+			transform: [
+				{ translateY: y },
+				// {scale: withTiming(pressed.value ? 1.1 : 1)}, 
+				// {height: withTiming(pressed.value ? 200 : AnimatedBoxHeightValue )}
+			],
+		}
+	})
 
-    return (
-        // <SafeAreaView>
-        //     <Controller
-        //         control={control}
-        //         rules={{ required: true }}
-        //         render={({ field: { onChange, onBlur, value } }) => (
-        //             <TextInput
-        //                 placeholder="Program Name"
-        //                 onBlur={onBlur}
-        //                 onChangeText={onChange}
-        //                 value={value}
-        //             />
-        //         )}
-        //         name="programName"
-        //     />
-        //     {errors.programName && <Text>Program name is needed.</Text>}
-        //     {/* button to add weeks */}
-        //     <Button onPress={handleAddWeek}>Add Week</Button>
-        //     {weekFields.map((week, weekIndex) => (
-        //         <TouchableOpacity key={week.id} onPress={() => {/* navigate to day screen */}}>
-        //         <Text>Week {weekIndex + 1}</Text>
-        //         </TouchableOpacity>
-        //         ))}
-        //     <Button onPress={handleSubmit(onSubmit)}>Submit</Button>
-        // </SafeAreaView>
-        <SafeAreaView>
-            <Button onPress={handleAddWeek}>Add Week</Button>
+	const animatedContentStyle = useAnimatedStyle(() => {
+		return {
+			opacity: pressed.value ? 1 : 0,
+			height: pressed.value ? 'auto' : 0,
+			paddingVertical: 15,
+		}
+	})
+
+	const handleDelete = () => {
+		onDelete()
+	}
+
+	const handleAdd = () => {
+		console.log('add')
+	}
+
+	const handleAddDay = () => {
+		setDays(prevDays => [...prevDays, {
+			key: generateUniqueId(),
+		}])
+	}
+
+	const renderItem = ({ item, drag, isActive }) => {
+		return (
+			<ScaleDecorator>
+				<AnimatedDayBox
+					box={item}
+					index={item.index}
+					onDelete={() => handleRemoveWeek(item)}
+					onDrag={drag}
+					isActive={isActive}
+				/>
+			</ScaleDecorator>
+		)
+	}
+
+	return (
+		<Animated.View
+			// className="border-2 border-rose-600" 
+			entering={FadeIn}
+			style={[styles.weekBox, animatedStyle]}
+		>
+			<GestureDetector gesture={tap}>
+				<TouchableOpacity
+					// className="border-2 border-blue-600" 
+					style={styles.gestureTapArea}
+					onLongPress={onDrag}
+					disabled={isActive}
+				>
+					<Text>Week {box.index + 1}</Text>
+				</TouchableOpacity>
+			</GestureDetector>
+
+			<Animated.View style={[animatedContentStyle]}>
+				<TouchableOpacity onLongPress={onDrag} disabled={isActive} >
+					<View style={styles.header}>
+						{/* <Button onPress={toggleExpand}>{expandedText}</Button> */}
+					</View>
+					<Button onPress={handleAddDay}>Add Day</Button>
             <DraggableFlatList
-                data={boxes}
-                onDragEnd={({ data }) => setBoxes(data)}
+                data={days}
+                onDragEnd={({ data }) => setDays(data)}
                 keyExtractor={(item) => item.key}
                 renderItem={renderItem}
             />
-            {/* <ScrollView>
-                {boxes.map((box, index) => (
-                    <AnimatedBox 
-                        key={box.id}
-                        index={index}
-                        box={box}
-                        onDelete={() => handleRemoveWeek(box)}
-                    />
-                ))}
-            </ScrollView> */}
+					<Button onPress={handleDelete}>Delete</Button>
+					<Button onPress={handleAdd}>add</Button>
+				</TouchableOpacity>
+			</Animated.View>
+		</Animated.View>
+	)
+}
 
-        </SafeAreaView>
-    )
+const CreateProgramScreen = () => {
+	// const { control, handleSubmit, formState: { errors } } = useForm({
+	//     defaultValues: {
+	//         programName: "",
+	//         weeks: []
+	//     }
+	// })
+
+	const onSubmit = (data) => console.log(data)
+
+	// const { fields: weekFields, append: appendWeek } = useFieldArray({
+	//     control,
+	//     name: "weeks"
+	// })
+
+	// Create an array to hold animated values for each week
+	// const weekAnimations = weekFields.map(() => useSharedValue(0))
+
+	// Function to append a week and initialize its animation
+	// const handleAddWeek = () => {
+	//     appendWeek({ days: [] })
+	//     appendWeek({ days: [] })
+	//     const newWeekAnimation = useSharedValue(0)
+	//     newWeekAnimation.value = withTiming(1, { duration: 500 })
+	//     weekAnimations.push(newWeekAnimation)
+	// }
+
+
+
+	const handleRemoveWeek = (boxToRemove) => {
+		const keyToRemove = boxToRemove.key
+		setBoxes((currentBoxes) => currentBoxes.filter((box, index) => box.key !== keyToRemove))
+	}
+
+	const handleAddWeek = () => {
+		const newBoxIndex = boxes.length
+		setBoxes(prevBoxes => [...prevBoxes, {
+			key: generateUniqueId(),
+			index: newBoxIndex,
+		}])
+		console.log(boxes)
+	}
+
+	const calculateNewBoxPosition = (index) => {
+		return index * 110
+	}
+
+	const [boxes, setBoxes] = useState(
+		[
+			{
+				key: generateUniqueId(),
+				index: 0,
+			}
+		]
+	)
+
+	const renderItem = ({ item, drag, isActive }) => {
+
+		return (
+			<ScaleDecorator>
+				<AnimatedWeekBox
+					box={item}
+					index={item.index}
+					onDelete={() => handleRemoveWeek(item)}
+					onDrag={drag}
+					isActive={isActive}
+				/>
+			</ScaleDecorator>
+		)
+	}
+
+	return (
+		// <SafeAreaView>
+		//     <Controller
+		//         control={control}
+		//         rules={{ required: true }}
+		//         render={({ field: { onChange, onBlur, value } }) => (
+		//             <TextInput
+		//                 placeholder="Program Name"
+		//                 onBlur={onBlur}
+		//                 onChangeText={onChange}
+		//                 value={value}
+		//             />
+		//         )}
+		//         name="programName"
+		//     />
+		//     {errors.programName && <Text>Program name is needed.</Text>}
+		//     {/* button to add weeks */}
+		//     <Button onPress={handleAddWeek}>Add Week</Button>
+		//     {weekFields.map((week, weekIndex) => (
+		//         <TouchableOpacity key={week.id} onPress={() => {/* navigate to day screen */}}>
+		//         <Text>Week {weekIndex + 1}</Text>
+		//         </TouchableOpacity>
+		//         ))}
+		//     <Button onPress={handleSubmit(onSubmit)}>Submit</Button>
+		// </SafeAreaView>
+		<SafeAreaView>
+			<Button onPress={handleAddWeek}>Add Week</Button>
+			<DraggableFlatList
+				data={boxes}
+				onDragEnd={({ data }) => setBoxes(data)}
+				keyExtractor={(item) => item.key}
+				renderItem={renderItem}
+			/>
+			{/* <ScrollView>
+				{boxes.map((box, index) => (
+					<AnimatedBox 
+						key={box.id}
+						index={index}
+						box={box}
+						onDelete={() => handleRemoveWeek(box)}
+					/>
+				))}
+			</ScrollView> */}
+
+		</SafeAreaView>
+	)
 }
 
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      alignItems: 'center',
-      justifyContent: 'center',
-      height: '100%',
-    },
-    box: {
-      height: 120,
-      width: 120,
-      backgroundColor: '#b58df1',
-      borderRadius: 20,
-      marginVertical: 50,
-    },
-    svg: {
-        height: 250,
-        width: '100%',
-    },
-    circle: {
-        height: 120,
-        width: 120,
-        borderRadius: 500
-    },
-    weekBox: {
-        height: AnimatedBoxHeightValue,
-        width: '90%',
-        // alignSelf: 'center',
-        // flexDirection: 'row',
+	container: {
+		flex: 1,
+		alignItems: 'center',
+		justifyContent: 'center',
+		height: '100%',
+	},
+	box: {
+		height: 120,
+		width: 120,
+		backgroundColor: '#b58df1',
+		borderRadius: 20,
+		marginVertical: 50,
+	},
+	svg: {
+		height: 250,
+		width: '100%',
+	},
+	circle: {
+		height: 120,
+		width: 120,
+		borderRadius: 500
+	},
+	weekBox: {
+		height: AnimatedBoxHeightValue,
+		width: '90%',
+		// alignSelf: 'center',
+		// flexDirection: 'row',
 
-        alignItems: 'center',
-        // justifyContent: 'left',
-        // backgroundColor: '#e0e0e0',
-        backgroundColor: 'lightwhite',
-        borderColor: 'black',
-        borderWidth: 1,
-        borderRadius: 10,
-        marginVertical: 10,
-        marginHorizontal: '5%',
-        // paddingVertical: 15,
-        overflow: 'hidden',
-    },
-    gestureTapArea: {
-        width: '100%',
-        height: 50, // Fixed height for the tap area
-        borderRadius: 10,
-        // marginVertical: -1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: '#e0e0e0',
-        borderBottomLeftRadius: 0,
-        borderBottomRightRadius: 0,
-    },
-    box: {
-        width: 100,
-        height: 100,
-        backgroundColor: 'blue',
-        margin: 5,
-    },
-    header: {
-        flexDirection: 'row',
-        alignItems: 'center',
-        justifyContent: 'space-between',
-    },
-    expandedContent: {
+		alignItems: 'center',
+		// justifyContent: 'left',
+		// backgroundColor: '#e0e0e0',
+		backgroundColor: 'lightwhite',
+		borderColor: 'black',
+		borderWidth: 1,
+		borderRadius: 10,
+		marginVertical: 10,
+		marginHorizontal: '5%',
+		// paddingVertical: 15,
+		overflow: 'hidden',
+	},
+	gestureTapArea: {
+		width: '100%',
+		height: 50, // Fixed height for the tap area
+		borderRadius: 10,
+		// marginVertical: -1,
+		justifyContent: 'center',
+		alignItems: 'center',
+		backgroundColor: '#e0e0e0',
+		borderBottomLeftRadius: 0,
+		borderBottomRightRadius: 0,
+	},
+	box: {
+		width: 100,
+		height: 100,
+		backgroundColor: 'blue',
+		margin: 5,
+	},
+	header: {
+		flexDirection: 'row',
+		alignItems: 'center',
+		justifyContent: 'space-between',
+	},
+	expandedContent: {
 
-    },
-  });
+	},
+});
 
 export default CreateProgramScreen;
