d813fd07ae8a683f44403c1d6b55abddd354890c
raymart-evangelista <raymart.s.evangelista@gmail.com>
2024-01-16 12:50:52 -0800

    add Screens to TabNav

--- src/navigation/TabNavigator.js
+++ src/navigation/TabNavigator.js
@@ -1,12 +1,17 @@
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import MainLoggedOut from "../../MainLoggedOut";
+import HomeScreen from "../screens/HomeScreen";
+import { ExistingProgramsScreen } from "../screens/ExistingProgramScreen";
 
 const Tab = createBottomTabNavigator()
 
 export default function TabNavigator() {
   return (
     <Tab.Navigator>
-      <Tab.Screen name='MainLoggedOut' component={MainLoggedOut} />
+      <Tab.Screen name='Home' component={HomeScreen} />
+      <Tab.Screen name='Programs' component={ExistingProgramsScreen} />
+      <Tab.Screen name='Log' component='' />
+      <Tab.Screen name='Leaderboards' component='' />
+      <Tab.Screen name='You' component='' />
     </Tab.Navigator>
   )
 }
