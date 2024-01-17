542060c8bd11cd9dc7cb088190c7a2bdb0046bd7
raymart-evangelista <raymart.s.evangelista@gmail.com>
2024-01-17 11:13:18 -0800

    add boilerplate screens

--- src/screens/FriendsScreen.js
+++ src/screens/FriendsScreen.js
@@ -1,8 +1,9 @@
 import { View } from "react-native";
 
-export default function ProfileScreen({ navigation }) {
+export default function FriendsScreen({ navigation }) {
   return (
     <View>
+
     </View>
   )
 }
