768b7af5ef0d14a764fe65ad310701c710cabbfd
raymart-evangelista <raymart.s.evangelista@gmail.com>
2024-02-04 18:26:21 -0800

    set up AddExercise feature

--- src/features/weeks/days/exercises/AddExercise.js
+++ src/features/weeks/days/exercises/AddExercise.js
@@ -1,40 +1,123 @@
-import { useState } from "react";
-import { useDispatch } from "react-redux";
-import { AnimatedFAB } from "react-native-paper";
-import { exerciseAdded } from "../../../weeksSlice";
-import { nanoid } from "@reduxjs/toolkit";
-import { StyleSheet } from "react-native";
+import { useState } from 'react'
+import { useDispatch } from 'react-redux'
+import {
+  AnimatedFAB,
+  Button,
+  Portal,
+  Modal,
+  TextInput,
+  Text,
+} from 'react-native-paper'
+import { exerciseAdded } from '../../../weeksSlice'
+import { nanoid } from '@reduxjs/toolkit'
+import { StyleSheet } from 'react-native'
+import { useForm, Controller } from 'react-hook-form'
 
 export const AddExercise = ({ weekId, dayId }) => {
-  const dispatch = useDispatch();
+  const dispatch = useDispatch()
+  const [visible, setVisible] = useState()
+  const showModal = () => setVisible(true)
+  const hideModal = () => setVisible(false)
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({})
 
+  // /**
+  //  * When a user is adding a new exercise, before dispatching, they must input the exercise info
+  //  * into the form, then after submitting, the input will be checked and then if everything
+  //  * is valid, it will be submitted
+  //  */
   const onAddExerciseClicked = () => {
+    showModal()
+  }
+
+  const onSubmit = (data) => {
     dispatch(
       exerciseAdded({
         weekId,
         dayId,
-        exercise: {},
-      }),
-    );
-  };
+        exercise: {
+          id: nanoid(),
+          name: data.name,
+          warmupSets: data.warmupSets,
+          workingSets: data.workingSets,
+          reps: data.reps,
+          RPE: data.RPE,
+        },
+      })
+    )
+    hideModal()
+  }
+
+  const onValidSubmission = () => {
+    dispatch(
+      exerciseAdded({
+        weekId,
+        dayId,
+        exercise: {
+          // data here
+        },
+      })
+    )
+  }
 
   return (
-    <AnimatedFAB
-      icon="plus"
-      label="Add Exercise"
-      onPress={onAddExerciseClicked}
-      animateFrom="right"
-      iconMode="dynamic"
-      style={[styles.fabStyle]}
-      extended="true"
-    />
-  );
-};
+    <>
+      <Portal>
+        <Modal
+          visible={visible}
+          onDismiss={hideModal}
+          contentContainerStyle={styles.containerStyle}
+        >
+          <Controller
+            control={control}
+            name="name"
+            rules={{ required: true }}
+            render={({ field: { onChange, onBlur, value } }) => (
+              <TextInput
+                label="Exercise Name"
+                onBlur={onBlur}
+                onChangeText={onChange}
+                value={value}
+                style={styles.input}
+              />
+            )}
+          />
+          {errors.name && <Text>Exercise name is required.</Text>}
+          {/* form stuff in here */}
+          <Button mode="contained" onPress={handleSubmit(onSubmit)}>
+            Submit
+          </Button>
+        </Modal>
+      </Portal>
+      <AnimatedFAB
+        icon="plus"
+        label="Add Exercise"
+        onPress={onAddExerciseClicked}
+        animateFrom="right"
+        iconMode="dynamic"
+        style={[styles.fabStyle]}
+        extended="true"
+      />
+    </>
+  )
+}
 
 const styles = StyleSheet.create({
   fabStyle: {
     bottom: 16,
     right: 16,
-    position: "absolute",
+    position: 'absolute',
   },
-});
+  containerStyle: {
+    backgroundColor: 'white',
+    padding: 20,
+    margin: 30,
+    borderRadius: 15,
+  },
+  input: {
+    marginBottom: 10,
+  },
+})
