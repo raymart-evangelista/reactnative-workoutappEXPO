72bc1b1f3e11d39c6ccb6a5f515d6e5ba4abacd0
raymart-evangelista <raymart.s.evangelista@gmail.com>
2024-04-08 23:41:37 -0700

    add programsSlice reducers

---
+++ src/features/programsSlice.js
@@ -0,0 +1,36 @@
+import { createSlice, nanoid } from '@reduxjs/toolkit'
+
+// this can be the user's programs
+const initialState = {
+  programs: [],
+}
+
+export const programsSlice = createSlice({
+  name: 'programs',
+  initialState,
+  reducers: {
+    addProgram: (state, action) => {
+      state.programs.push({ ...action.payload, id: nanoid() })
+    },
+    updateProgram: (state, action) => {
+      const { id, ...updates } = action.payload
+      const program = state.programs.find((program) => program.id === id)
+      if (program) {
+        for (const key in updates) {
+          // program[key] = updates[key]
+          // can also be written as
+          Object.assign(program, updates)
+        }
+      }
+    },
+    deleteProgram: (state, action) => {
+      state.programs = state.programs.filter(
+        (program) => program.id !== action.payload.id
+      )
+    },
+  },
+})
+
+export const { addProgram, updateProgram, deleteProgram } =
+  programsSlice.actions
+export default programsSlice.reducer
