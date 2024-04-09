1c13de082e47c8792ee80d57e9136059c7ba358e
raymart-evangelista <raymart.s.evangelista@gmail.com>
2024-04-08 23:47:01 -0700

    add programsSlice to store

--- src/app/store.js
+++ src/app/store.js
@@ -2,10 +2,12 @@ import { configureStore } from '@reduxjs/toolkit'
 
 import weeksReducer from '../features/program/weeksSlice'
 import programReducer from '../features/programSlice'
+import programsReducer from '../features/programsSlice'
 
 export default configureStore({
   reducer: {
     program: programReducer,
     weeks: weeksReducer,
+    programs: programsReducer,
   },
 })
