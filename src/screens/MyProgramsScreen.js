e99e17ba67db571ae574abc2a93a18770a651419
raymart-evangelista <raymart.s.evangelista@gmail.com>
2024-04-09 00:04:19 -0700

    add MyProgramsScreen with redirect form CreateProgramScreen

---
+++ src/screens/MyProgramsScreen.js
@@ -0,0 +1,10 @@
+import { View } from 'react-native'
+import { Text } from 'react-native-paper'
+
+export default function MyProgramsScreen({ navigation }) {
+  return (
+    <View>
+      <Text>This screen shows your programs</Text>
+    </View>
+  )
+}
