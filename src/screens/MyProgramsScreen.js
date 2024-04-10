de5f167cf9544a496cb48837294e024dcc993701
raymart-evangelista <raymart.s.evangelista@gmail.com>
2024-04-10 11:20:58 -0700

    add MyProgramsScreen boiler plate

--- src/screens/MyProgramsScreen.js
+++ src/screens/MyProgramsScreen.js
@@ -1,10 +1,32 @@
 import { View } from 'react-native'
+import { FlatList } from 'react-native-gesture-handler'
 import { Text } from 'react-native-paper'
+import { useSelector } from 'react-redux'
 
 export default function MyProgramsScreen({ navigation }) {
   return (
     <View>
       <Text>This screen shows your programs</Text>
+      <ProgramList />
     </View>
+  )
+}
+
+const ProgramList = () => {
+  const programs = useSelector((state) => state.programs.programs)
+
+  const renderItem = ({ item }) => (
+    <View style={{ flex: 1, padding: 20 }}>
+      <Text>{item.title}</Text>
+      <Text>{item.description}</Text>
+    </View>
+  )
+
+  return (
+    <FlatList
+      data={programs}
+      renderItem={renderItem}
+      keyExtractor={(item) => item.id}
+    />
   )
 }
