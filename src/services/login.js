e1b8f47d16b97621da4239cfad07a094ab8ca6ec
raymart-evangelista <raymart.s.evangelista@gmail.com>
2024-04-15 12:39:47 -0700

    remove semicolons and throw error from backend

--- src/services/login.js
+++ src/services/login.js
@@ -1,9 +1,17 @@
-import axios from "axios";
-const baseUrl = "http://localhost:3000/api/login";
+import axios from 'axios'
+const baseUrl = 'http://localhost:3000/api/login'
 
 const login = async (credentials) => {
-  const res = await axios.post(baseUrl, credentials);
-  return res.data;
-};
+  try {
+    const res = await axios.post(baseUrl, credentials)
+    return res.data
+  } catch (error) {
+    if (error.response && error.response.data) {
+      throw new Error(error.response.data.error || 'Login failed')
+    } else {
+      throw new Error('Login failed due to network or system error')
+    }
+  }
+}
 
-export default { login };
+export default { login }
