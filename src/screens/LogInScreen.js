c4b2a14fb246a6a849697ee8a1cc4bc5257f7ea7
raymart-evangelista <raymart.s.evangelista@gmail.com>
2024-04-16 11:59:49 -0700

    reconstruct LogInScreen to utilize react hook form

--- src/screens/LogInScreen.js
+++ src/screens/LogInScreen.js
@@ -1,16 +1,36 @@
 import { useState, useEffect, useContext } from 'react'
-import { Button, Text, TextInput, TouchableOpacity, View } from 'react-native'
-import { defaultStyles } from '../styles/globalStyles'
+import { Text, TouchableOpacity, View } from 'react-native'
+import { TextInput, Button } from 'react-native-paper'
+import { defaultStyles, useThemedStyles } from '../styles/globalStyles'
 import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view'
 
 import loginService from '../services/login'
 import Notification from '../components/Notification'
 import { AuthContext } from '../contexts/AuthContext'
 
+import { useForm, Controller } from 'react-hook-form'
+import { SafeAreaView } from 'react-native-safe-area-context'
+import { yupResolver } from '@hookform/resolvers/yup'
+import * as yup from 'yup'
+const loginSchema = yup.object({
+  username: yup.string().required('* Username is required'),
+  password: yup.string().required('* Password is required'),
+})
+
 export default function LogInScreen({ navigation }) {
-  const [username, setUsername] = useState('')
-  const [password, setPassword] = useState('')
-  const [user, setUser] = useState(null)
+  const styles = useThemedStyles()
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(loginSchema),
+    mode: 'onSubmit',
+    defaultValues: {
+      username: '',
+      password: '',
+    },
+  })
   const [loading, setLoading] = useState(false)
 
   const [notificationMessage, setNotificationMessage] = useState('')
@@ -18,30 +38,29 @@ export default function LogInScreen({ navigation }) {
 
   const { login } = useContext(AuthContext)
 
-  const handleLogin = async () => {
-    if (username === '' || password === '') {
-      setNotificationMessage('All fields are required')
-      setNotificationColor('red')
-      return
-    }
+  // const handleLogin = async () => {
+  //   if (username === '' || password === '') {
+  //     setNotificationMessage('All fields are required')
+  //     setNotificationColor('red')
+  //     return
+  //   }
 
+  const onLoginSubmit = async ({ username, password }) => {
     try {
       setLoading(true)
       const user = await loginService.login({
         username,
         password,
       })
-      setUser(user)
-
       setNotificationMessage(`Success. Welcome ${user.username}.`)
       setNotificationColor('green')
-      navigation.replace('TabNavigator', { screen: 'Home' })
-      // updates AuthContext
-
+      navigation.reset({
+        index: 0,
+        routes: [{ name: 'TabNavigator', params: { screen: 'Home' } }],
+      })
       login({ user: user.username, email: user.email, token: user.token })
     } catch (error) {
       console.error(error)
-      // setNotificationMessage('Log in failed. Wrong credentials')
       setNotificationMessage(error.message)
       setNotificationColor('red')
     } finally {
@@ -49,39 +68,74 @@ export default function LogInScreen({ navigation }) {
     }
   }
 
-  // useEffect(() => {
-  //   if (user) {
-  //     navigation.navigate("Home")
-  //   }
-  // }, [user])
-
   return (
-    <KeyboardAwareScrollView contentContainerStyle={defaultStyles.basic}>
+    <SafeAreaView
+      className="border-2 border-red-500"
+      // contentContainerStyle={defaultStyles.basic}
+      style={styles.screenWithOptions}
+    >
+      <Text className="text-center text-6xl">PeakPlanner</Text>
+      <Text style={defaultStyles.signupText}>Log In</Text>
+      <Notification message={notificationMessage} color={notificationColor} />
+
+      <Controller
+        control={control}
+        name="username"
+        render={({
+          field: { onChange, onBlur, value },
+          fieldState: { error },
+        }) => (
+          <View style={styles.inputContainer}>
+            <TextInput
+              label="Username"
+              onBlur={onBlur}
+              onChangeText={onChange}
+              value={value || ''}
+              error={!!error}
+              mode="outlined"
+              autoCapitalize="none"
+              style={styles.input}
+            />
+          </View>
+        )}
+      />
+      {errors.username && (
+        <Text style={styles.errorText}>{errors.username.message}</Text>
+      )}
+      <Controller
+        control={control}
+        name="password"
+        render={({
+          field: { onChange, onBlur, value },
+          fieldState: { error },
+        }) => (
+          <View style={styles.inputContainer}>
+            <TextInput
+              label="Password"
+              onBlur={onBlur}
+              onChangeText={onChange}
+              value={value || ''}
+              error={!!error}
+              mode="outlined"
+              autoCapitalize="none"
+              style={styles.input}
+              secureTextEntry
+            />
+          </View>
+        )}
+      />
+      {errors.password && (
+        <Text style={styles.errorText}>{errors.password.message}</Text>
+      )}
+      <Button
+        mode="contained"
+        style={styles.button}
+        onPress={handleSubmit(onLoginSubmit)}
+      >
+        {loading ? <Text>Loading</Text> : <Text>Log In</Text>}
+      </Button>
       <View style={defaultStyles.basic}>
-        <Text className="text-center text-6xl">PeakPlanner</Text>
-        <Notification message={notificationMessage} color={notificationColor} />
-        {/* <Text style={defaultStyles.signupText}>Log In</Text> */}
-        <View style={{ marginHorizontal: 24 }}>
-          <TextInput
-            style={defaultStyles.signupInput}
-            placeholder="Username"
-            onChangeText={(newUsername) => setUsername(newUsername)}
-            autoCapitalize="none"
-            autoCorrect={false}
-          />
-        </View>
-        <View style={{ marginHorizontal: 24 }}>
-          <TextInput
-            style={defaultStyles.signupInput}
-            placeholder="Password"
-            onChangeText={(newPassword) => setPassword(newPassword)}
-            autoCapitalize="none"
-            autoCorrect={false}
-            autoComplete="password"
-            secureTextEntry={true}
-          />
-        </View>
-        <TouchableOpacity
+        {/* <TouchableOpacity
           onPress={handleLogin}
           style={[defaultStyles.buttonStyle, user !== null && { opacity: 0.5 }]}
           disabled={user !== null}
@@ -91,7 +145,7 @@ export default function LogInScreen({ navigation }) {
           ) : (
             <Text style={defaultStyles.buttonText}>Log In</Text>
           )}
-        </TouchableOpacity>
+        </TouchableOpacity> */}
         <Text
           style={{
             color: 'black',
@@ -107,6 +161,6 @@ export default function LogInScreen({ navigation }) {
           Forgot password?
         </Text>
       </View>
-    </KeyboardAwareScrollView>
+    </SafeAreaView>
   )
 }
