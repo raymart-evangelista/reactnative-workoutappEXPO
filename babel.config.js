236ce45dd195749d5bb68866d5ec2113704c5f78
raymart-evangelista <raymart.s.evangelista@gmail.com>
2024-05-09 13:02:42 -0700

    install watermelondb

--- babel.config.js
+++ babel.config.js
@@ -1,12 +1,16 @@
 module.exports = function (api) {
-  api.cache(true);
+  api.cache(true)
   return {
-    presets: ["babel-preset-expo"],
+    presets: ['babel-preset-expo'],
     env: {
       production: {
-        plugins: ["react-native-paper/babel"],
+        plugins: ['react-native-paper/babel'],
       },
     },
-    plugins: ["nativewind/babel", "react-native-reanimated/plugin"],
-  };
-};
+    plugins: [
+      'nativewind/babel',
+      'react-native-reanimated/plugin',
+      ['@babel/plugin-proposal-decorators', { legacy: true }],
+    ],
+  }
+}
