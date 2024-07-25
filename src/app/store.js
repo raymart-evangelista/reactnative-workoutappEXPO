d839bf9762c92c0fd2fb6e340fb938011c57c4b9
raymart-evangelista <raymart.s.evangelista@gmail.com>
2024-07-24 20:07:54 -0700

    add userReducer

--- src/app/store.js
+++ src/app/store.js
@@ -3,11 +3,13 @@ import { configureStore } from '@reduxjs/toolkit'
 import weeksReducer from '../features/program/weeksSlice'
 import programReducer from '../features/programSlice'
 import programsReducer from '../features/programsSlice'
+import userReducer from '../features/userSlice'
 
 export default configureStore({
   reducer: {
     program: programReducer,
     weeks: weeksReducer,
     programs: programsReducer,
+    user: userReducer,
   },
 })
