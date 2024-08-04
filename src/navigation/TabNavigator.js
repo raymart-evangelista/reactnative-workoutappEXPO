86e578564df6af48d9fc9ec1a223870cfdb600b0
raymart-evangelista <raymart.s.evangelista@gmail.com>
2024-08-03 22:37:15 -0700

    refactor TabNavigator, remove ExistingProgramScreen.js from Tab nav stack

--- src/navigation/TabNavigator.js
+++ src/navigation/TabNavigator.js
@@ -5,59 +5,71 @@ import ProfileScreen from '../screens/ProfileScreen'
 import FriendsScreen from '../screens/FriendsScreen'
 import { useTheme } from '../themes/ThemeContext'
 import HomeStackNavigator from './HomeNavigator'
+import ProgramsStackNavigator from './ProgramsNavigator'
 
 const Tab = createBottomTabNavigator()
 
 export default function TabNavigator() {
   const { theme } = useTheme()
+
   return (
     <Tab.Navigator
       screenOptions={{
         tabBarActiveTintColor: theme.colors.primary,
         tabBarInactiveTintColor: theme.colors.onBackground,
         tabBarStyle: { backgroundColor: theme.colors.background },
+        headerShown: false,
       }}
     >
       <Tab.Screen
         name="Programs"
-        component={ExistingProgramsStackScreen}
-        options={{
-          headerShown: true,
-          headerTintColor: theme.colors.onBackground,
-        }}
+        component={ProgramsStackNavigator}
+        options={
+          {
+            // headerShown: true,
+            // headerTintColor: theme.colors.onBackground,
+          }
+        }
       />
       <Tab.Screen
         name="Home"
         component={HomeStackNavigator}
-        options={{
-          headerShown: true,
-          headerTintColor: theme.colors.onBackground,
-        }}
+        options={
+          {
+            // headerShown: true,
+            // headerTintColor: theme.colors.onBackground,
+          }
+        }
       />
-      {/* <Tab.Screen name="Programs" component={ExistingProgramsScreen} /> */}
       <Tab.Screen
         name="Log"
         component=""
-        options={{
-          headerShown: true,
-          headerTintColor: theme.colors.onBackground,
-        }}
+        options={
+          {
+            // headerShown: true,
+            // headerTintColor: theme.colors.onBackground,
+          }
+        }
       />
       <Tab.Screen
         name="Friends"
         component={FriendsScreen}
-        options={{
-          headerShown: true,
-          headerTintColor: theme.colors.onBackground,
-        }}
+        options={
+          {
+            // headerShown: true,
+            // headerTintColor: theme.colors.onBackground,
+          }
+        }
       />
       <Tab.Screen
-        name="You"
+        name="Profile"
         component={ProfileScreen}
-        options={{
-          headerShown: true,
-          headerTintColor: theme.colors.onBackground,
-        }}
+        options={
+          {
+            // headerShown: true,
+            // headerTintColor: theme.colors.onBackground,
+          }
+        }
       />
     </Tab.Navigator>
   )
