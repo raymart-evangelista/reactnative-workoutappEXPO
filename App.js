d74f3721d1eaf35815aa97eab6533fc5dd16b807
raymart-evangelista <raymart.s.evangelista@gmail.com>
2024-08-07 23:07:48 -0700

    refactor Auth nav to AuthStack, clean up

--- App.js
+++ App.js
@@ -55,7 +55,7 @@ const Stack = createNativeStackNavigator()
 import { GestureHandlerRootView } from 'react-native-gesture-handler'
 import TabNavigator from './src/navigation/TabNavigator'
 
-import { Provider as ReduxProvider } from 'react-redux'
+import { Provider as ReduxProvider, useSelector } from 'react-redux'
 import store from './src/app/store'
 
 import { SafeAreaProvider } from 'react-native-safe-area-context'
@@ -105,33 +105,23 @@ export default function App() {
   )
 }
 
+function AuthStack() {
+  return (
+    <Stack.Navigator screenOptions={{ headerShown: false }}>
+      <Stack.Screen name="Landing" component={LandingPage} />
+      <Stack.Screen name="LogIn" component={LogInScreen} />
+      <Stack.Screen name="SignUp" component={SignUpScreen} />
+    </Stack.Navigator>
+  )
+}
+
 function AppContent() {
   const { theme } = useTheme()
-  const isAuthenticated = true
+  const isAuthenticated = useSelector((state) => state.auth.isAuthenticated)
 
   return (
     <NavigationContainer theme={theme}>
-      {/* <Stack.Navigator initialRouteName='CreateProgram' screenOptions={{headerShown: true}}> */}
-      <Stack.Navigator
-        initialRouteName="Landing"
-        // screenOptions={{ headerShown: false }}
-      >
-        {isAuthenticated ? (
-          <Stack.Screen
-            name="TabNavigator"
-            component={TabNavigator}
-            options={{ headerShown: false }}
-          />
-        ) : (
-          <>
-            <Stack.Screen name="Landing" component={LandingPage} />
-            <Stack.Screen name="LogIn" component={LogInScreen} />
-            <Stack.Screen name="SignUp" component={SignUpScreen} />
-          </>
-        )}
-        {/* <Stack.Screen name="Settings" component={SettingsScreen} /> */}
-        {/* <Stack.Screen name="ProgramDetails" component={ProgramDetailsScreen} /> */}
-      </Stack.Navigator>
+      {isAuthenticated ? <TabNavigator /> : <AuthStack />}
     </NavigationContainer>
   )
 }
